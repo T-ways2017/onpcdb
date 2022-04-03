de601156a765c0176bf4aa34ad52237d9621271a
T-ways2017 <ingenieur.wesley@gmail.com>
2022-04-03 10:11:47 +0000

    add on firefighter migration the foreigns keys and edit center model

--- app/Models/Center.php
+++ app/Models/Center.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\firefighter;
+
 
 class Center extends Model
 {
     use HasFactory;
+
+    //One Center has many CFiregthters
+
+    public function firefighters()
+    {
+        return $this->hasMany('App\Models\Firefighter');
+
+    }
 }
+
+
--- database/migrations/2022_03_30_112506_create_firefighters_table.php
+++ database/migrations/2022_03_30_112506_create_firefighters_table.php
@@ -20,11 +20,37 @@ class CreateFirefightersTable extends Migration
             $table->date('birthday');
             $table->string('sex',1);
             $table->string('email')->unique();
+            $table->string('phone1');
+            $table->string('phone2');
             $table->string('cv');
             $table->string('rib');
             $table->string('picture');
-            $table->string('more');
-            
+            $table->string('more'); 
+            //foreign keys Center
+            $table->unsignedBigInteger('center_id');
+            $table->foreign('center_id')
+            ->reference('id')
+            ->on('centers')
+            ->onDelete('restrict')
+            ->onUpdate('restrict');   
+            //other method
+            //$table->foreignId('center_id')->constrained();
+            //       
+            //foreign keys Department
+            $table->unsignedBigInteger('department_id');
+            $table->foreign('departement_id')
+            ->reference('id')
+            ->on('departments')
+            ->onDelete('restrict')
+            ->onUpdate('restrict');  
+
+            //foreign keys Category
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')
+            ->reference('id')
+            ->on('categories')
+            ->onDelete('restrict')
+            ->onUpdate('restrict');  
 
             $table->timestamps();
         });
